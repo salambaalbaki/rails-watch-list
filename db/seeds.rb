226476d8b17c2c7e249817895a187cacf451e402
Salam Baalbaki <salambaalbaki@gmail.com>
2022-02-18 10:43:29 +0400

    began building movies watch list app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+
+Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+
+Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+
+
+# Comedy
+
+Movie.create(title: "Meet The Parents", overview: "Meeting your partner’s parents is bound to be stressful – but Greg Focker (Ben Stiller) has it worse than most.", poster_url: "https://www.themoviedb.org/t/p/w1280/5tXJ9ctuyEOMUFLaeqRisbXowWs.jpg", rating: 7.4)
+
+Movie.create(title: "Dumb and Dumber", overview: "Jim Carrey and Jeff Daniels star as a pair of stupendously stupid no-hopers who head on a road trip across America to return a woman’s briefcase.", poster_url: "https://www.themoviedb.org/t/p/w1280/4LdpBXiCyGKkR8FGHgjKlphrfUc.jpg", rating: 9.0)
+
+Movie.create(title: "Ghostbusters", overview: "When New York is invaded by ghastly ghouls, who you gonna call? You know the answer: four self-styled Ghostbusters ready to dash in and zap the spirits into oblivion.", poster_url: "https://www.themoviedb.org/t/p/w1280/di9t2YVpWDKuUtYkrm10yVld50x.jpg", rating: 8.0)
+
+
+# Romance
+
+Movie.create(title: "The Notebook", overview: "Based on a novel by Nicholas Sparks, this romance spans decades, flipping between a couple's first blush of love in the '40s and the present-day reality of how that relationship has transformed in old age.", poster_url: "https://www.themoviedb.org/t/p/w1280/rNzQyW4f8B8cQeg7Dgj3n6eT5k9.jpg", rating: 9.5)
+
+Movie.create(title: "Eternal Sunshine of the Spotless Mind", overview: "Eternal Sunshine of the Spotless Mind breaks all the rules for romances, starting after the central couple has already broken up.", poster_url: "https://www.themoviedb.org/t/p/w1280/5MwkWH9tYHv3mV9OdYTMR5qreIz.jpg", rating: 8.2)
+
+Movie.create(title: "Moulin Rouge", overview: "Love is a many splendid thing. Love lifts us up where we belong. All you need is love.", poster_url: "https://www.themoviedb.org/t/p/w1280/elj5J1p09UdO1eqjfQuIafSpftp.jpg", rating: 8.7)
+
+# Classic
+
+Movie.create(title: "The Wizard of Oz", overview: "There’s no place like home, and no classic movie as beloved as Dorothy’s adventures in Oz.", poster_url: "https://www.themoviedb.org/t/p/w1280/bSA6DbAC5gdkaooU164lQUX6rVs.jpg", rating: 9.5)
+
+Movie.create(title: "Breakfast At Tiffany's", overview: "A favorite of dorm-room posters and Halloween costumes, Blake Edwards’s comedy about girl-about-town Holly Golightly helped turn Audrey Hepburn into a fashion icon, thanks to her long black gown, elegant up-do, and signature black sunglasses.", poster_url: "https://www.themoviedb.org/t/p/w1280/aQFwKTNzp02rk0GFRAjTdmbZBNh.jpg", rating: 9.7)
+
+Movie.create(title: "Pyscho", overview: "Alfred Hitchcock’s experiment with exploitation cinema proved the Master of Suspense to be the Master of Marketing, and became his most notorious film in a career of classics.", poster_url: "https://www.themoviedb.org/t/p/w1280/5j5tdV8tMTy7FBdSNiYxp9Fj1Or.jpg", rating: 8.5)
